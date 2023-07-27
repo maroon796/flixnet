d6d4b990bed2587833a3ac79267cb27e715d395b
Grygory Ksheminsky <maroon785@gmail.com>
2023-07-27 23:44:06 +0300

    Working on rows of the movies

--- src/Requests.js
+++ src/Requests.js
@@ -4,7 +4,6 @@ const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
   requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-  // requestHorror: `https://api.themoviedb.org/3/movie?api_key=${key}&language=en-US&page=1`,
   requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
 };
 
